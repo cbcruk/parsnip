19c9d8f9a172ceb2880e388aa705d34f9cdcd9d0
Eunsoo Lee <cbcruk@users.noreply.github.com>
2022-05-30 21:54:51 +0900

    feat: 상세페이지 JSON-LD 추가

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -13,7 +13,30 @@ function ArticleId({ data }) {
     <>
       <Head>
         <title>다른마켓 - {article.title}</title>
-        <meta name="description" content="당근과는 다르다! 당근과는!" />
+        <meta name="description" content={article.content} />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org/',
+              '@type': 'Product',
+              name: article.title,
+              image: article.images.map((image) => image.file),
+              description: article.content,
+              offers: {
+                '@type': 'Offer',
+                url: `https://parsnip-three.vercel.app/articles/${article.id}`,
+                priceCurrency: 'KRW',
+                price: article.price,
+                itemCondition: 'https://schema.org/UsedCondition',
+                availability:
+                  article.status === 'ongoing'
+                    ? 'https://schema.org/InStock'
+                    : 'https://schema.org/SoldOut',
+              },
+            }),
+          }}
+        />
       </Head>
 
       <Article article={article} />
