39996fa4cff06802f7a1a9f426e0f7869fb94ba7
Eunsoo Lee <cbcruk@users.noreply.github.com>
2022-06-05 19:30:40 +0900

    fix: <title /> 텍스트 추가

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { SearchList } from '../../components/List/Search'
 import { useSearch } from '../../components/List/Search/useSearch'
 import { useSearchHistory } from '../../components/List/Search/useSearchHistory'
@@ -5,11 +7,15 @@ import Sticky from '../../components/Sticky'
 import { TopAppBars } from '../../components/TopAppBars'
 
 function Search() {
+  const router = useRouter()
   const { data, isValidating, size, setSize } = useSearch()
   useSearchHistory()
 
   return (
     <>
+      <Head>
+        <title>{router.query.q} - 다른마켓 검색</title>
+      </Head>
       <Sticky>
         <TopAppBars title="검색" />
       </Sticky>
